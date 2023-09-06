3806b6aa61b82dd0fd8ba60807f4bd9b40ff071c
TranHuynhTanPhat <tranhuynhtanphat9380@gmail.com>
2023-09-06 23:47:39 +0700

    create unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,28 +5,45 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:todo_list/blocs/bloc_tasks/tasks_bloc.dart';
+import 'package:todo_list/blocs/cubit_res/res_cubit.dart';
 import 'package:todo_list/main.dart';
 import 'package:todo_list/services/app_route.dart';
 
 void main() {
+  ResCubit resCubit;
+  TasksBloc tasksBloc;
+  Storage storage;
+
+  setUp(() {
+    storage = MockStorage();
+    when(
+      () => storage.write(any(), any<dynamic>()),
+    ).thenAnswer((_) async {});
+    HydratedBloc.storage = storage;
+    resCubit = ResCubit();
+    tasksBloc = TasksBloc();
+  });
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( MyApp(appRouter: AppRouter()));
+    await tester.pumpWidget(MyApp(appRouter: AppRouter()));
 
     // // Verify that our counter starts at 0.
     // expect(find.text('0'), findsOneWidget);
     // expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // // Tap the '+' icon and trigger a frame.
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
 
     // // Verify that our counter has incremented.
     // expect(find.text('0'), findsNothing);
     // expect(find.text('1'), findsOneWidget);
   });
 }
+
+class MockStorage extends Mock implements Storage {}
