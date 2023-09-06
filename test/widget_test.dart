fe4d4ee13c3e55c853dfd35e46ef4f8a5ec61feb
TranHuynhTanPhat <tranhuynhtanphat9380@gmail.com>
2023-09-06 19:33:09 +0700

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:todo_list/main.dart';
 import 'package:todo_list/services/app_route.dart';
@@ -20,9 +21,9 @@ void main() {
     // expect(find.text('0'), findsOneWidget);
     // expect(find.text('1'), findsNothing);
 
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
 
     // // Verify that our counter has incremented.
     // expect(find.text('0'), findsNothing);
